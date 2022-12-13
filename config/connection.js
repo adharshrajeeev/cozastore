f1d42427579048d9c39513c788474e75439b7f27
Adharsh R <adharshrajeeev2000@gmail.com>
2022-12-13 10:49:21 +0530

    second

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=(done)=>{
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://adharsh:test123@cluster0.jbrekb1.mongodb.net/?retryWrites=true&w=majority'
     const dbname='ecommerce'
 
     mongoClient.connect(url,(err,data)=>{
